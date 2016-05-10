bb14f853ec82aec29b129b67ba56fc9c51b860c0
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2016-05-10 01:08:57 -0400

    Added name pronunciation

--- js/index.js
+++ js/index.js
@@ -16,4 +16,44 @@ $(document).ready(function () {
     } else {
         $('#learnMore').remove();
     }
+    
+    var html5_audiotypes = {
+        mp3: "audio/mpeg",
+        mp4: "audio/mp4",
+        ogg: "audio/ogg",
+        wav: "audio/wav"
+    };
+    
+    function createsoundbite(d) {
+        var b = document.createElement("audio");
+        if (b.canPlayType) {
+            for (var c = 0; c < arguments.length; c++) {
+                var a = document.createElement("source");
+                a.setAttribute("src", arguments[c]);
+                if (arguments[c].match(/\.(\w+)$/i)) {
+                    a.setAttribute("type", html5_audiotypes[RegExp.$1])
+                }
+                b.appendChild(a)
+            }
+            b.load();
+            b.playclip = function() {
+                b.pause();
+                b.currentTime = 0;
+                b.play()
+            }
+            ;
+            return b
+        } else {
+            return {
+                playclip: function() {
+                    throw new Error("Your browser doesn't support HTML5 audio!")
+                }
+            }
+        }
+    }
+    var name = createsoundbite("./files/Ze.mp3");
+
+    $("#short-name").click(function () {
+        name.playclip();
+    });
 });
